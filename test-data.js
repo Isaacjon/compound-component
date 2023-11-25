5fee4732ba8835aaa8445dba90d201b45e68b2ca
isaac <izzatazimov958@gmail.com>
2023-11-25 20:37:19 +0500

    third test

---
+++ test-data.js
@@ -0,0 +1,109 @@
+export const lavash = [
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "39 000",
+    isAvailable: true,
+    img: "/images/lavash1.png",
+  },
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "49 000",
+    isAvailable: true,
+    img: "/images/lavash2.png",
+  },
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "29 000",
+    isAvailable: false,
+    img: "/images/pizza1.png",
+  },
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "19 000",
+    isAvailable: true,
+    img: "/images/lavash1.png",
+  },
+];
+
+export const burger = [
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "39 000",
+    isAvailable: true,
+    img: "/images/burger1.png",
+  },
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "49 000",
+    isAvailable: true,
+    img: "/images/burger2.png",
+  },
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "29 000",
+    isAvailable: false,
+    img: "/images/burger1.png",
+  },
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "19 000",
+    isAvailable: true,
+    img: "/images/burger2.png",
+  },
+];
+
+export const sandwich = [
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "39 000",
+    isAvailable: true,
+    img: "/images/sandwich1.png",
+  },
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "49 000",
+    isAvailable: true,
+    img: "/images/sandwich1.png",
+  },
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "29 000",
+    isAvailable: false,
+    img: "/images/sandwich1.png",
+  },
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "19 000",
+    isAvailable: true,
+    img: "/images/sandwich1.png",
+  },
+];
+
+export const shaurma = [
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "39 000",
+    isAvailable: true,
+    img: "/images/shaurma1.png",
+  },
+  {
+    title: "Комбо",
+    subtitle: "Мясной лаваш, фри, кола разлив и кетчуп",
+    price: "49 000",
+    isAvailable: true,
+    img: "/images/shaurma1.png",
+  },
+];
